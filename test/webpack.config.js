7d071076b1ba5fc74e4978735802768ebd8199fd
Johannes Ewald <mail@johannesewald.de>
2013-03-18 11:49:46 +0100

    - removed unnecessary style- and css-loader

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -3,12 +3,6 @@ var RewirePlugin = require("../");
 module.exports = {
     entry: "mocha!./webpack.test.js",
     devtool: "eval",
-    module: {
-        loaders: [{
-            test: /\.css$/,
-            loader: "style-loader!css-loader"
-        }]
-    },
     plugins: [
         new RewirePlugin()
     ]
